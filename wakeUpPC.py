3d794a5725d9206b68758c7788ad1d5f394bfb49
Kosei <pinochi0202@gmail.com>
2023-11-18 02:16:41 +0900

    update

--- wakeUpPC.py
+++ wakeUpPC.py
@@ -4,14 +4,14 @@ import binascii
 
 def wake_on_lan(macaddress):
     # MACアドレスをバイナリ形式に変換
-    macaddress = macaddress.replace(":", "").replace("-", "")
+    macaddress = macaddress.replace(':', '').replace('-', '')
     macaddress = binascii.unhexlify(macaddress)
 
     # Wake-on-LANマジックパケットの作成
-    magic_packet = b"\xff" * 6 + macaddress * 16
+    magic_packet = b'\xff' * 6 + macaddress * 16
 
-    # 対象のMacBookのIPアドレスとポート番号
-    target_address = ("192.168.11.1", 8080)
+    # 対象のPCのIPアドレスとポート番号
+    target_address = ('192.168.11.6', 9)
 
     # マジックパケットを送信
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -19,5 +19,5 @@ def wake_on_lan(macaddress):
 
 
 # 対象PCのMACアドレスを指定
-target_mac = "08:f8:bc:6c:9f:29"
+target_mac = "04:7C:16:80:01:E7"
 wake_on_lan(target_mac)
